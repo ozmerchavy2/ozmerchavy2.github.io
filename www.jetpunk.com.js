db7097072a926e67f4d9a6398967d67d570872dd
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-18 16:07:43 -0230

    Update www.jetpunk.com.js

--- www.jetpunk.com.js
+++ www.jetpunk.com.js
@@ -1,10 +1,11 @@
-sade = document.querySelector('input[name="stopitchrome"]');
+
+function guess() {
+    sade = document.querySelector('input[name="stopitchrome"]');
 countries = []
 for (const answer in (_page.data.recommendedTypeins)){
     countries.push(answer)}
 let i = 0;
 
-function guess() {
     if (i < countries.length) {
         sade.value = countries[i];
         sade.dispatchEvent(new KeyboardEvent("input"));
