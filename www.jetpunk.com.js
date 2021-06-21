d56d85c43465766e927d302b85a2343a8077d7eb
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-21 18:49:36 -0230

    Update www.jetpunk.com.js

--- www.jetpunk.com.js
+++ www.jetpunk.com.js
@@ -2,11 +2,18 @@
 function all() {
     sade = document.querySelector('input[name="stopitchrome"]');
 countries = _page.data.quiz.answers.map(x=> x.display)
+answers = countries.map(x => {
+    if (!x.includes(`{`)) {
+        return x
+    } else {
+        return x.split("{")[1].split("}")[0]
+    }
+})
 let i = 0
 function guess(){
     
-    if (i < countries.length) {
-        sade.value = countries[i];
+    if (i < answers.length) {
+        sade.value = answers[i];
         sade.dispatchEvent(new KeyboardEvent("input"));
         i++;
         setTimeout(guess, 15);
