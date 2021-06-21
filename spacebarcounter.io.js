a823f662369fff37bbf1b3dc15c1a813d8521eed
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-21 19:20:14 -0230

    Update spacebarcounter.io.js

--- spacebarcounter.io.js
+++ spacebarcounter.io.js
@@ -1,1 +1,4 @@
-document.addEventListener("keydown", ()=>{ hits += Math.floor(Math.random()*125 +12); setCookie('hits', hits, 365); addHit(); })
+document.addEventListener("keyup", (e)=>{ 
+  var keyCode = e.keyCode || e.charCode;
+if (keyCode != 32) return;
+ hits += Math.floor(Math.random()*125 +12); setCookie('hits', hits, 365); addHit(); })
