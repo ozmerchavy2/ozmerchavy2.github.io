e302733153d48583d4b6e47b453f11722a3b2fdc
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-29 17:05:31 -0230

    Delete www.youtube.com.js

--- www.youtube.com.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var videoElement = document.querySelector("video");
-    var audioCtx = new AudioContext();
-    var source = audioCtx.createMediaElementSource(videoElement);
-    var gainNode = audioCtx.createGain();
-    gainNode.gain.value = 2
-    source.connect(gainNode);
-    gainNode.connect(audioCtx.destination);
-})();
