4fbd072eedee3ccc11c6eaa6f4b9c193f3627f45
Oz <83187947+ozmerchavy2@users.noreply.github.com>
2021-06-29 17:06:39 -0230

    Delete spacebarcounter.io.js

--- spacebarcounter.io.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("keyup", (e)=>{ 
-  var keyCode = e.keyCode || e.charCode;
-if (keyCode != 32) return;
- hits += Math.floor(Math.random()*125 +12); setCookie('hits', hits, 365); addHit(); })
